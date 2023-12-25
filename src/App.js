50772d47b0cbddecc5ff48ef07701c817cc3e247
Brian-kipkoech-mutai <kbrianmutai@gmail.com>
2023-12-25 14:35:19 +0300

    adding packages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-
 import { Provider } from "react-redux";
- import store from "./redux/store";
- import MarkedDownComponent from "./containers/MarkedDownComponent";
+import store from "./redux/store";
+import MarkedDownComponent from "./containers/MarkedDownComponent";
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
- import '../src/styling/markDown.css'
+import '../src/styling/markDown.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
